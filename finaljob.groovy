ef9a1b49c081b0f6940b10e5cb332e179d42a4ef
abhishreepavan <72576507+abhishreepavan@users.noreply.github.com>
2022-05-31 21:48:45 +0530

    Update finaljob.groovy

--- finaljob.groovy
+++ finaljob.groovy
@@ -1,7 +1,23 @@
-pipelineJob('product-job') {
+pipelineJob('aws') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline jobs/ansible_pipeline_Jenkinsfile'))
+      sandbox()     
+    }
+  }
+}
+pipelineJob('buidling docker image and pushing it to ECR') {
   definition {
     cps {
       script(readFileFromWorkspace('pipeline jobs/build_job_jenkinsfile'))
+      sandbox()     
+    }
+  }
+}
+pipelineJob('running a docker image') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline jobs/deploy_job_jenkinsfile'))
       sandbox()     
     }
   }
